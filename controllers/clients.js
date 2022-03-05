ffbfae9aa025c3e25115021ec315fa653a86d132
sarahpicard <sarahannrey225@gmail.com>
2022-03-04 20:54:31 -0500

    start create controller function

--- controllers/clients.js
+++ controllers/clients.js
@@ -19,7 +19,31 @@ function newClient(req, res) {
   })
 }
 
+function show(req, res) {
+  Client.findById(req.params.id).populate('owner').then(client => {
+    res.render('clients/show', {
+      client,
+      title: 'Client'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/clients')
+  })
+}
+
+function create(req, res) {
+  req.body.status = !!req.body.status
+  const client = new Client(req.body)
+  client.save(function(err) {
+    if (err) return res.redirect('/clients/new')
+    res.redirect('/clients')
+  })
+}
+
 export {
   index,
   newClient as new, 
+  show,
+  create,
 }
--- routes/clients.js
+++ routes/clients.js
@@ -9,6 +9,12 @@ router.get('/', clientsCtrl.index)
 // GET localhost:3000/clients/new
 router.get('/new', clientsCtrl.new)
 
+// GET localhost:3000/clients/:id
+router.get('/:id', clientsCtrl.show)
+
+// POST localhost:3000/clients
+router.post('/', clientsCtrl.create)
+
 export {
   router,
 }
